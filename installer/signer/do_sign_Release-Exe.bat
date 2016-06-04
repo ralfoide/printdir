48402b68791e0b72be9847160ab04140d966a88a
ralf <ralfoide@gmail.com>
2016-06-04 12:23:18 -0700

    Remove the outdated NSI installer

--- installer/signer/do_sign_Release-Exe.bat
+++
@@ -1,2 +0,0 @@
-signcode -n "PrintDir XP Installer" -i http://www.powerpulsar.com -spc mycredentials.spc -v myprivatekey.pvk -a sha1 -t http://timestamp.verisign.com/scripts/timstamp.dll ..\..\PrintDirXP\bin\Release\PrintDirXP.exe
-
